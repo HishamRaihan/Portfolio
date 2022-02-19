eb5e97d412783c95d43ef363f25e24b7701211b0
hishamraihan <mr.hraihan@gmail.com>
2022-02-18 19:51:30 -0500

    finished intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './intro.scss'
+  import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
+
+
+export default function Intro() {
+  
+  // works like document and queryselector
+  const textRef = useRef()
+
+  useEffect(() =>{
+    init(textRef.current, { 
+      showCursor: true, 
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Project Manager', 'Software Developer', 'Designer'], 
+    })
+  },[])
+  return (
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/portrait.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Hisham Raihan</h1>
+          <h3>What I Do: <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+}
